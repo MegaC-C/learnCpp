c401ad523fdcce172f514cc57e7c6a4d3580c621
root <root@localhost>
2021-08-13 11:36:07 +0200

    primesSiebFertig

--- learnCpp/learnCpp.cpp
+++ learnCpp/learnCpp.cpp
@@ -15,18 +15,21 @@ int main() {
 	cin >> max;
 	for(int i=2; i<=max;++i) nums.push_back(i);
 	
-	for(int i=0; i<nums.size();++i) cout << nums[i]<<endl;
+	//for(int i=0; i<nums.size();++i) cout << nums[i]<<endl;
 	
 	for (int a=0; a<nums.size()-1; ++a){
+		
 		for (int i=1;i<nums.size();++i){
-			if(nums[a+i]%nums[a]==0){
-				nums.erase(nums.begin()+i);
+			if((i+a)<nums.size() && nums[a+i]%nums[a]==0){
+				nums.erase(nums.begin()+(i+a));
 				--i;
 			}
 		}
+		
+		
 	}
-	for(int i=0; i<nums.size();++i) cout << nums[i]<<endl;
-	
+
+		for(int j=0; j<nums.size();++j) cout << nums[j]<<endl;
 
 }
 
